9d0e69c1d0cc1c457ab778fcecbac7ab51c8d291
Mat Teubert <mat@pixelbreakfast.com>
2014-02-05 18:29:41 +1100

    Initial Commit

---
+++ Boat People/Assets/Scripts/Rock.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Rock : MonoBehaviour {
+	
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+		transform.localRotation = Quaternion.Euler(Mathf.Sin(Time.realtimeSinceStartup) * 12,0,0);
+
+	}
+
+}
