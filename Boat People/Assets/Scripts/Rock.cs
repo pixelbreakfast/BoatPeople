6b51e9b979c409f1f7cd591dd059befc3b5dd2c3
Mat Teubert <mat@pixelbreakfast.com>
2014-02-05 19:05:01 +1100

    Refactored title screen

--- Boat People/Assets/Scripts/Rock.cs
+++ Boat People/Assets/Scripts/Rock.cs
@@ -2,16 +2,22 @@
 using System.Collections;
 
 public class Rock : MonoBehaviour {
-	
+
+	Quaternion startRotation;
+	public Vector3 rotations;
+
 	// Use this for initialization
 	void Start () {
-	
+		startRotation = transform.rotation;
 	}
 	
 	// Update is called once per frame
 	void Update () {
 	
-		transform.localRotation = Quaternion.Euler(Mathf.Sin(Time.realtimeSinceStartup) * 12,0,0);
+		transform.localRotation = startRotation * Quaternion.Euler(
+			Mathf.Sin(Time.realtimeSinceStartup) * rotations.x,
+			Mathf.Sin(Time.realtimeSinceStartup) * rotations.y,
+			Mathf.Sin(Time.realtimeSinceStartup) * rotations.z);
 
 	}
 
